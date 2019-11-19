f891cfc0ef0667fc113f3702f8162cf31fbbdd5a
K <shaty3@hotmail.com>
2019-11-19 15:02:23 +0900

    再帰処理からネスト情報をスタックするように変更

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace SimpleRPN
@@ -7,9 +8,9 @@ namespace SimpleRPN
     {
         static void Main(string[] args)
         {
-            var data = "- 1 + 2 * 3 + 4 * ( 5 + 1 )".Split(' ');
+            var dat = new Lex("- 1 + 2 *3 + 4 * ( 5 + 1 )");
             var rpn = new RPN();
-            foreach (var str in data)
+            foreach (var str in dat)
             {
                 rpn.Update(str);
             }
@@ -19,266 +20,178 @@ namespace SimpleRPN
         }
     }
 
-    /// <summary>
-    /// 逆ポーランド
-    /// </summary>
+    public class Lex : IEnumerable<string>
+    {
+        readonly string Data;
+        int Index = 0;
+        int Length => Data.Length;
+        char Current => Data[Index];
+
+        bool IsBusy => Index < Length;
+
+        bool MoveNext()
+        {
+            Index++;
+            return IsBusy;
+        }
+
+        public Lex(string data)
+        {
+            Data = data;
+        }
+
+        bool IsNumeric0(char ch) => (ch == '+' || ch == '-' || ('0' <= ch && ch <= '9'));
+        bool IsNumeric1(char ch) => ('0' <= ch && ch <= '9');
+        bool IsKigo(char ch) => (ch == '(' || ch == ')' || ch == '*' || ch == '/');
+        bool IsSpace(char ch) => (ch == ' ');
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            while (IsBusy)
+            {
+                var result = "";
+                var ch = Current;
+                if (IsSpace(ch))
+                {
+                    Index++;
+                    continue;
+                }
+                else if (IsNumeric0(ch))
+                {
+                    result += ch;
+                    while (MoveNext())
+                    {
+                        ch = Current;
+                        if (IsSpace(ch))
+                        {
+                            continue;
+                        }
+                        else if (IsNumeric1(ch))
+                        {
+                            result += ch;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                else if (IsKigo(ch))
+                {
+                    result += ch;
+                    Index++;
+                }
+                yield return result;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public class NestInfo
+    {
+        public Symbol Symbol;
+        public int Pos;
+
+        public NestInfo(Symbol symbol, int pos)
+        {
+            Symbol = symbol;
+            Pos = pos;
+        }
+    }
+
     public class RPN
     {
-        readonly Stack<RPNInfo> Stacks = new Stack<RPNInfo>();
-
-        RPNInfo Current;
-
-        public RPN()
-        {
-            Current = new RPNInfo();
-        }
+        readonly Stack<NestInfo> Nests = new Stack<NestInfo>();
+        readonly Stack<int> values = new Stack<int>();
+        Symbol Symbol;
+        bool IsCalc = false;
 
         public void Update(string msg)
         {
-            var sym = (Symbol)msg[0];
-            switch(sym)
+            switch(msg[0])
             {
-                case Symbol.KakkoStart:
-                    Stacks.Push(Current);
-                    Current = new RPNInfo();
+                case '(':
+                    Nests.Push(new NestInfo((IsCalc) ? Symbol : Symbol.None, values.Count + 1));
+                    IsCalc = false;
                     break;
 
-                case Symbol.KakkoEnd:
-                    var result = Current.GetResult();
-                    Current = Stacks.Pop();
-                    Current.Update(result.ToString());
+                case ')':
+                    var info = Nests.Pop();
+                    if (Symbol.None != info.Symbol)
+                    {
+                        Eval(info.Pos);
+                        var r = info.Symbol.Calc(values.Pop(), values.Pop());
+                        values.Push(r);
+                    }
                     break;
 
                 default:
-                    Current.Update(msg);
+                    if (int.TryParse(msg, out int num))
+                    {
+                        values.Push(num);
+                        if (IsCalc)
+                        {
+                            IsCalc = false;
+                            values.Push(Symbol.Calc(values.Pop(), values.Pop()));
+                        }
+                    }
+                    else
+                    {
+                        if (IsCalc)
+                        {
+                            throw new Exception("");
+                        }
+                        else
+                        {
+                            IsCalc = true;
+                            Symbol = (Symbol)msg[0];
+                        }
+                    }
                     break;
             }
         }
 
         public int GetResult()
         {
-            if (0 == Stacks.Count)
+            Eval(1);
+            return values.Pop();
+        }
+
+        void Eval(int stopPos)
+        {
+            while (stopPos < values.Count)
             {
-                return Current.GetResult();
-            }
-            else
-            {
-                throw new Exception("");
+                if (IsCalc)
+                {
+                    IsCalc = false;
+                    values.Push(Symbol.Calc(values.Pop(), values.Pop()));
+                }
+                else
+                {
+                    values.Push(values.Pop() + values.Pop());
+                }
             }
         }
     }
 
-    /// <summary>
-    /// RPN
-    /// </summary>
-    public class RPNInfo
-    {
-        int L;
-        int R;
-        Symbol Symbol1;
-        Symbol Symbol2;
-        int State;  //0,2(偶数)=数字,  {1,3}(基数)=記号
-        bool MinusHugo = false;
-
-        /// <summary>
-        /// 値取得
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
-        int GetValue(int value)
-        {
-            if (MinusHugo)
-            {
-                MinusHugo = false;
-                return -value;
-            }
-            else
-            {
-                return value;
-            }
-        }
-
-        /// <summary>
-        /// 数字の符号かチェック
-        /// </summary>
-        /// <param name="sym"></param>
-        void CheckHugo(Symbol sym)
-        {
-            switch (sym)
-            {
-                case Symbol.Plus:
-                    break;
-
-                case Symbol.Minus:
-                    MinusHugo = true;
-                    break;
-
-                default:
-                    throw new Exception("");
-            }
-        }
-
-        /// <summary>
-        /// 計算
-        /// </summary>
-        /// <param name="msg"></param>
-        public void Update(string msg)
-        {
-            bool isNumeric = int.TryParse(msg, out int val);
-            var num = GetValue(val);
-            var sym = (Symbol)msg[0];
-
-            //数字だった
-            switch (State)
-            {
-                case 0: //1番目数字
-                    if (isNumeric)
-                    {
-                        L = num;
-                        State++;
-                    }
-                    else
-                    {
-                        CheckHugo(sym);
-                    }
-                    break;
-
-                case 1: //左辺記号
-                    if (isNumeric)
-                    {
-                        throw new Exception("");
-                    }
-                    else
-                    {
-                        Symbol1 = sym;
-                        State++;
-                    }
-                    break;
-
-                case 2: //2番目数字
-                    if (isNumeric)
-                    {
-                        if (Symbol1.IsHighPriority())
-                        {
-                            L = Symbol1.Calc(L, num);
-                            State--;
-                        }
-                        else
-                        {
-                            R = num;
-                            State++;
-                        }
-                    }
-                    else
-                    {
-                        CheckHugo(sym);
-                    }
-                    break;
-
-                case 3: //右辺記号
-                    if (isNumeric)
-                    {
-                        throw new Exception("");
-                    }
-                    else
-                    {
-                        if (sym.IsLowPriority())
-                        {
-                            L = Symbol1.Calc(L, R);
-                            Symbol1 = sym;
-                            State--;
-                        }
-                        else
-                        {
-                            Symbol2 = sym;
-                            State++;
-                        }
-                    }
-                    break;
-
-                case 4: //3番目数字(高優先度のみ)
-                    if (isNumeric)
-                    {
-                        R = Symbol2.Calc(R, num);
-                        State--;
-                    }
-                    else
-                    {
-                        CheckHugo(sym);
-                    }
-                    break;
-
-                default:
-                    throw new Exception("");
-            }
-        }
-
-        /// <summary>
-        /// 計算結果取得
-        /// </summary>
-        /// <returns></returns>
-        public int GetResult()
-        {
-            if (State == 3)
-            {
-                L = Symbol1.Calc(L, R);
-                State--;
-                return L;
-            }
-            else if (2 == State)
-            {
-                return L;
-            }
-            else
-            {
-                throw new Exception("");
-            }
-        }
-    }
-
-
-    /// <summary>
-    /// 演算記号
-    /// </summary>
     public enum Symbol
     {
-        Plus = '+',
-        Minus = '-',
+        None = 0,
         Mul = '*',
         Div = '/',
-        KakkoStart = '(',
-        KakkoEnd = ')',
     }
-    //TODO：ここをフラグ列挙型にして、文字列パーサを用意する
 
-    /// <summary>
-    /// 演算記号拡張メソッド
-    /// </summary>
     public static class SymbolExtensions
     {
         public static int Calc(this Symbol self, int l, int r)
         {
             switch (self)
             {
-                case Symbol.Plus:
-                    return l + r;
-
-                case Symbol.Minus:
-                    return l - r;
-
-                case Symbol.Mul:
-                    return l * r;
-
-                case Symbol.Div:
-                    return l / r;
-
-                default:
-                    throw new Exception();
+                case Symbol.Mul:    return l * r;
+                case Symbol.Div:    return l / r;
+                default:            throw new Exception();
             }
         }
-
-        public static bool IsLowPriority(this Symbol s) => (Symbol.Plus == s || Symbol.Minus == s);
-        public static bool IsHighPriority(this Symbol s) => (Symbol.Mul == s || Symbol.Div == s);
     }
-
 }
